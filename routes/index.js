de9fe66c63809ea03f3294136fd17a44e4c71e2c
Sangeeta <saangeeeeta@gmail.com>
2020-10-26 07:18:37 +0530

    The Intial Commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+
+
+
+
+router.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+
+
+
+module.exports = router;
